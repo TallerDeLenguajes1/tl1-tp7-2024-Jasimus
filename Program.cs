d0a8a6cd9c8adde24e5088f300c994591d260ece
Gonzalo Javier Martinez <goncham94@gmail.com>
2024-05-21 12:33:18 -0300

    6to-commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Huluo, uorud!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Huluo, uorud!");
+    }
+}
