12d57984b057209a75566029e215a0d41f7a4361
Juan Augusto Adad <juanadadbatero181@gmail.com>
2024-05-31 15:25:32 -0300

    resolución punto 1

--- Program.cs
+++ Program.cs
@@ -2,13 +2,45 @@
 using EspacioCalculadora;
 
 double valor;
-string valorS;
+int  oper, seguir;
+string valorS, operS, seguirS;
+Calculadora calc1 = new Calculadora();
 do{
+    Console.WriteLine("valor actual: "+calc1.Resultado);
     Console.WriteLine("Qué operación desea hacer:\n1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir");
-    valorS = Console.ReadLine();
-    if(TryParse(valorS, out valor));
+    operS = Console.ReadLine();
+    if(int.TryParse(operS, out oper));
     {
-        
+        do
+        {
+            Console.WriteLine("ingrese el valor a operar: ");
+            valorS = Console.ReadLine();
+
+        }while(!double.TryParse(valorS, out valor));
+
+        switch(oper)
+        {
+            case 1:
+                calc1.Sumar(valor);
+                break;
+            case 2:
+                calc1.Restar(valor);
+                break;
+            case 3:
+                calc1.Multiplicar(valor);
+                break;
+            case 4:
+                calc1.Dividir(valor);
+                break;
+            default:
+                break;
+        }
+        Console.WriteLine("valor actual: "+calc1.Resultado);
     }
-}while();
+    do 
+    {
+        Console.WriteLine("desea hacer otra operación? 1.SI 0.NO");
+        seguirS = Console.ReadLine();
+    }while(!int.TryParse(seguirS, out seguir));
+}while(seguir!=0);
 
